4ca25c7a040ace91da833264d51ad685bd15e085
matthoward01 <92449533+matthoward01@users.noreply.github.com>
2021-10-13 10:36:31 -0400

    Add files via upload

---
+++ File Mover/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using ExcelLibrary.SpreadSheet;
+
+namespace File_Mover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button1.BackColor = System.Drawing.Color.Red;
+            string searchPath = textBox1.Text;
+            string movePath = textBox2.Text;
+            string xlsPath = textBox5.Text;
+
+            
+            Workbook book = Workbook.Load(xlsPath);
+            Worksheet sheet = book.Worksheets[0];
+            int rowCount = sheet.Cells.Rows.Count();
+            string[] jobs = new string[rowCount];
+            for (int i = 0; i < rowCount; i++)
+            {
+                
+                jobs[i] = sheet.Cells[i, 0].StringValue;
+                
+            }
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            int jobcount = jobs.Count();
+            progressBar1.Step = (100 / jobcount);
+            foreach (string j in jobs){ 
+                string job = j;
+
+                string sourceFile = System.IO.Path.Combine(searchPath, job+".pdf");
+                string destFile = System.IO.Path.Combine(movePath, job + ".pdf");
+
+                if (!System.IO.Directory.Exists(movePath))
+                {
+                    System.IO.Directory.CreateDirectory(movePath);
+                }
+
+                System.IO.File.Copy(sourceFile, destFile, true);
+
+            }
+            textBox3.Text += "DONE!";
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button1.BackColor = System.Drawing.Color.Green;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            //ofd.Filter = "Comma Delimited|*.csv";
+            ofd.Filter = "Excel File|*.xls";
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string filename = "";
+                filename = ofd.FileName;
+                textBox5.Text = filename;
+                progressBar1.Value = 0;
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult dr = fbd.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                string folderpath = "";
+                folderpath = fbd.SelectedPath;
+                textBox1.Text = folderpath;
+                progressBar1.Value = 0;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult dr = fbd.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                string folderpath = "";
+                folderpath = fbd.SelectedPath;
+                textBox2.Text = folderpath;
+                progressBar1.Value = 0;
+            }
+        }
+    }
+}
